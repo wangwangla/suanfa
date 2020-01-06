bca78c92aea0f540b4ef155c6cf54feee44ba872
wangwangla <2818815189@qq.com>
2020-01-06 23:32:41 +0800

    算法提交

--- src/kw/jianxiansiqi/Demo.java
+++ src/kw/jianxiansiqi/Demo.java
@@ -50,5 +50,50 @@ public class Demo {
         return sum(arr,lo,mi)+sum(arr,mi+1,hi);
     }
 
+    //计算出一个数组中的两个最大值和次大值
+    /**
+     * 实现思路就是，先指定两个数，一个最大一个最小
+     * 
+     * 遍历数据，求出最大的值，并且更新x1和x2
+     */
+    public void find(int arr[],int lo ,int hi ,int x1,int x2){
+        if(arr[lo]>arr[lo+1]){
+            x1 = arr[lo];
+            x2 = arr[lo+1]
+        }else{
+            x1 = arr[lo+1];
+            x2 = arr[lo];
+        }
+        for(int i=2;i<arr.length;i++){
+            if(x2>arr[i]){
+                x2 = arr[i];
+                if(x1<x2){
+                    int temp = x1;
+                    x1 = x2;
+                    x2 = temp;
+                }
+            }
+        }
+    }
 
+
+    //递归的写法
+    public int fbnq(int n){
+        return (n<2)?0:fbnq(n)+fbnq(n-1);
+    }
+
+
+
+    //fbnq
+    //时间复杂度  n
+    public int fun(){
+        f = 0;
+        g = 1;
+        int n = 10;
+        while(0 < n--){
+            g = g + f;
+            f = g - f;
+        }
+        return g;
+    }
 }
